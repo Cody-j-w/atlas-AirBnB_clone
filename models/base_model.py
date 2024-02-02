f37a8db9ad67654df1df369d1ce19a21e8709f2d
Cody <codywalenciak@gmail.com>
2024-02-02 10:11:46 -0600

    Modify __init__ method to accept kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,21 @@ class BaseModel:
 
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         timestr = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == 'created_at':
+                    self.created_at = datetime.strptime(kwargs[key], timestr)
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(kwargs[key], timestr)
+                elif key == '__class__':
+                    pass
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         name = type(self).__name__
