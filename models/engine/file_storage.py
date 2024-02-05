2b0d95744e5bbc10d5f8333c69dc112d1783a166
Cody <codywalenciak@gmail.com>
2024-02-05 12:32:04 -0600

    Add else to FileStorage.new() if statement

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,8 @@ class FileStorage:
     def new(self, obj):
         if type(obj) is not dict:
             obj_dict = obj.to_dict()
-        obj_dict = obj
+        else:
+            obj_dict = obj
         key = "{0}.{1}".format(obj_dict['__class__'], obj_dict['id'])
         self.__objects.update({key: obj_dict})
 
