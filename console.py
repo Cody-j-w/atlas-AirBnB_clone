f61d2101631c6768948fb5d228284d09b63cbd97
Cody <codywalenciak@gmail.com>
2024-02-07 14:36:48 -0600

    Adjust indentation in console

--- console.py
+++ console.py
@@ -124,15 +124,15 @@ class HBNBCommand(cmd.Cmd):
             args_list = args.split(" ")
             if args_list[0] not in classes:
                 print("** class doesn't exist **")
-            if len(args_list) < 2:
-                print("** instance id missing **")
-            else:
-                key = "{}.{}".format(args_list[0], args_list[1])
-                if key not in storage.all():
-                    print("** no instance found **")
+                if len(args_list) < 2:
+                    print("** instance id missing **")
                 else:
-                    del storage.all()[key]
-                    storage.save()
+                    key = "{}.{}".format(args_list[0], args_list[1])
+                    if key not in storage.all():
+                        print("** no instance found **")
+                    else:
+                        del storage.all()[key]
+                        storage.save()
 
     def do_update(self, args):
         """
@@ -145,29 +145,29 @@ class HBNBCommand(cmd.Cmd):
             args_list = args.split(" ")
             if args_list[0] not in classes:
                 print("** class doesn't exist **")
-            if len(args_list) < 2:
-                print("** instance id missing **")
-            else:
-                key = "{}.{}".format(args_list[0], args_list[1])
-                if key not in storage.all():
-                    print("** no instance found **")
+                if len(args_list) < 2:
+                    print("** instance id missing **")
                 else:
-                    obj = storage.all()[key]
-                    if len(args_list) < 3:
-                        print("** attribute name missing **")
+                    key = "{}.{}".format(args_list[0], args_list[1])
+                    if key not in storage.all():
+                        print("** no instance found **")
                     else:
-                        attr_name = args_list[2]
-                        if len(args_list) < 4:
-                            print("** value missing **")
+                        obj = storage.all()[key]
+                        if len(args_list) < 3:
+                            print("** attribute name missing **")
                         else:
-                            attr_val = args_list[3]
-                            if hasattr(storage.all()[key], attr_name):
-                                attrtype = type(getattr(obj, attr_name))
-                                value = attrtype(attr_val)
-                                setattr(obj, attr_name, value)
-                                obj.save()
+                            attr_name = args_list[2]
+                            if len(args_list) < 4:
+                                print("** value missing **")
                             else:
-                                print("** attribute not found")
+                                attr_val = args_list[3]
+                                if hasattr(storage.all()[key], attr_name):
+                                    attrtype = type(getattr(obj, attr_name))
+                                    value = attrtype(attr_val)
+                                    setattr(obj, attr_name, value)
+                                    obj.save()
+                                else:
+                                    print("** attribute not found")
 
 
     def do_quit(self, arg):
