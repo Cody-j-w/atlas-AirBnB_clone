332a57dd78bd210c235b25eb40165b449d39942f
Cody <codywalenciak@gmail.com>
2024-02-07 15:53:48 -0600

    Modify base_model tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -27,6 +27,3 @@ class TestBaseMethods(unittest.TestCase):
     def test_created_at(self):
         b1 = BaseModel()
         self.assertEqual(b1.created_at, datetime.now())
-
-if __name__ == '__main__':
-    unittest.main()
